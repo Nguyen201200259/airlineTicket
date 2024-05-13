2c00205f30396f9a7ae71c9f1d0bf6bb79cfd126
Nguyen201200259 <nguyenwon0902@gmail.com>
2024-05-13 11:51:14 +0700

    update

---
+++ src/test/java/com/kttt/webbanve/WebBanVeApplicationTests.java
@@ -0,0 +1,28 @@
+package com.kttt.webbanve;
+
+
+import com.kttt.webbanve.repositories.OrderRepositories;
+import com.kttt.webbanve.services.GeneratePdfService;
+import com.kttt.webbanve.services.MailSenderService;
+import com.kttt.webbanve.services.TicketService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+@SpringBootTest
+class WebBanVeApplicationTests {
+	@Autowired
+	GeneratePdfService generatePdfService;
+
+	@Autowired
+	MailSenderService mailSenderService;
+
+	@Test
+	void contextLoads() throws Exception {
+
+	}
+}
